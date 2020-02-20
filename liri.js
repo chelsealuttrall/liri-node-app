c702f0be115095fa71a7257656798568baec2af7
chelsealuttrall <chelsealuttrall@gmail.com>
2020-02-19 21:54:14 -0700

    Creating LiriBot

---
+++ liri.js
@@ -0,0 +1,22 @@
+js
+require("dotenv").config();
+
+js
+var keys = require("./keys.js");
+
+js
+var spotify = new Spotify(keys.spotify);
+
+// 9. Make it so liri.js can take in one of the following commands:
+
+//     *
+//     `concert-this`
+
+// *
+// `spotify-this-song`
+
+// *
+// `movie-this`
+
+// *
+// `do-what-it-says`
