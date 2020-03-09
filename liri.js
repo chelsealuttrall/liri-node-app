0826b0d497250fb4db76edadb5ea1f1fe72ea60d
chelsealuttrall <chelsealuttrall@gmail.com>
2020-03-08 18:27:38 -0600

    added node packages

--- liri.js
+++ liri.js
@@ -18,7 +18,8 @@ var spotify = new Spotify(keys.spotify);
 
 // * [Axios](https://www.npmjs.com/package/axios)
 
-//   * You'll use Axios to grab data from the [OMDB API](http://www.omdbapi.com) and the [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
+//   * You'll use Axios to grab data from the [OMDB API](http://www.omdbapi.com) 
+//and the [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
 
 // * [Moment](https://www.npmjs.com/package/moment)
 
@@ -27,36 +28,6 @@ var spotify = new Spotify(keys.spotify);
 
 
 // ### Instructions
-
-
-
-
-/
-// 5. Make a file called `random.txt`.
-
-//    * Inside of `random.txt` put the following in with no extra characters or white space:
-
-//      * spotify-this-song,"I Want it That Way"
-
-// 6. Make a JavaScript file named `liri.js`.
-
-// 7. At the top of the `liri.js` file, add code to read and set any environment variables with the dotenv package:
-
-// ```js
-// require("dotenv").config();
-// ```
-
-// 8. Add the code required to import the `keys.js` file and store it in a variable.
-
-// ```js
-//   var keys = require("./keys.js");
-// ```
-
-// * You should then be able to access your keys information like so
-
-//   ```js
-//   var spotify = new Spotify(keys.spotify);
-//   ```
 
 // 9. Make it so liri.js can take in one of the following commands:
 
